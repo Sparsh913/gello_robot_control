1147c13c4ee85e509628472a63d1eaf18efd010d
Sparsh Garg <sparshg@andrew.cmu.edu>
2024-02-05 17:12:45 -0500

    Revert "Added my port np.pi + 0 * np.pi,"
    This reverts commit 54d7335bc080dc568b3c1e6834c4646d333f6b00.
    
    Wrong commit message

--- gello/agents/gello_agent.py
+++ gello/agents/gello_agent.py
@@ -103,20 +103,6 @@ PORT_CONFIG_MAP: Dict[str, DynamixelRobotConfig] = {
         joint_signs=(1, 1, -1, 1, 1, 1),
         gripper_config=(7, 286, 248),
     ),
-    # Custom UR
-    "/dev/serial/by-id/usb-FTDI_USB__-__Serial_Converter_FT8ISNEF-if00-port0": DynamixelRobotConfig(
-        joint_ids=(1, 2, 3, 4, 5, 6),
-        joint_offsets=(
-            np.pi + 0 * np.pi,
-            2 * np.pi + np.pi / 2,
-            2 * np.pi + np.pi / 2,
-            2 * np.pi + np.pi / 2,
-            1 * np.pi,
-            3 * np.pi / 2,
-        ),
-        joint_signs=(1, 1, -1, 1, 1, 1),
-        gripper_config=(7, 286, 248),
-    ),
 }
 
 
